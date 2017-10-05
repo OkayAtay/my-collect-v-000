95f18cde523e0be4175754631e9b06052ce51569
Araba Taylor <araba.taylor20@gmail.com>
2017-10-05 22:50:33 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(empty_array) do |x|
   i = 0
   new_array = []
-  while i < array.length
-    new_array<<yield(array[i])
+  while i < empty_array.length
+    new_array<<yield(empty_array[i])
     i += 1
   end
   new_array
